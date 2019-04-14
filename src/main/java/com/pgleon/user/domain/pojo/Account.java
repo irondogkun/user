6188a5060d35bc301d6fe611067b2ee6d1dfd962
Your Name <you@example.com>
2019-04-14 20:52:38 +0800

    test

--- src/main/java/com/pgleon/user/domain/pojo/Account.java
+++ src/main/java/com/pgleon/user/domain/pojo/Account.java
@@ -5,7 +5,6 @@ package com.pgleon.user.domain.pojo;
  */
 public class Account {
     private Integer userId;
-
     private String userName;
     private String password;
     private Integer roleId;
